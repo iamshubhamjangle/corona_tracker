8387ef3d6d93f3a0069bacec49aecfcbca1e7883
imshubham <jangleshubham@ymail.com>
2020-07-29 18:10:08 +0530

    Added MPAndroidChart

--- app/src/main/java/com/octa/coronatracker/MainActivity.java
+++ app/src/main/java/com/octa/coronatracker/MainActivity.java
@@ -2,22 +2,34 @@ package com.octa.coronatracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_sync;
     TextView textView;
-    int active, delActive, discharged, delDischarged, death, delDeath;
+    String[] casesDataArray;
+    PieChart pieChart;
+    ArrayList<String> xVals = new ArrayList<>();
+    ArrayList<Entry> yvalues = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +38,18 @@ public class MainActivity extends AppCompatActivity {
 
         btn_sync = findViewById(R.id.btn_sync);
         textView = findViewById(R.id.textView);
+        pieChart = findViewById(R.id.pie_chart);
+
+        setupPieChart();
 
         btn_sync.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("Mytag", "Button Clicked");
                 getWebsite();
                 splitTheText();
+                updatePieChart();
             }
         });
-
-
-
 
     }
 
@@ -60,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
+                        textView.setText(builder);
                     }
                 });
             }
@@ -67,15 +80,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void splitTheText() {
-        Log.d("Mytag", "Split the text started");
-        String[] casesDataArr = textView.getText().toString().split(" ");
+        Log.d("Mytag", "split the text Processing started");
+        Log.d("Mytag", textView.getText().toString());
+        casesDataArray = textView.getText().toString().split(" ");
+        Log.d("Mytag", "Values added to the arrayList");
+    }
 
-        active = Integer.parseInt(casesDataArr[0]);
-        delActive = Integer.parseInt(casesDataArr[1].substring(1, casesDataArr[1].length()-1));
-        discharged = Integer.parseInt(casesDataArr[2]);
-        delDischarged = Integer.parseInt(casesDataArr[3].substring(1, casesDataArr[3].length()-1));
-        death = Integer.parseInt(casesDataArr[4]);
-        delDeath = Integer.parseInt(casesDataArr[5].substring(1, casesDataArr[5].length()-1));
+    private void setupPieChart() {
+        yvalues.add(new Entry(1, 0));
+        yvalues.add(new Entry(1, 1));
+        yvalues.add(new Entry(1, 2));
 
+        xVals.add("Active");
+        xVals.add("Recovered");
+        xVals.add("Death");
+
+        PieDataSet dataSet = new PieDataSet(yvalues, getString(R.string.chart_label));
+        PieData data = new PieData(xVals, dataSet);
+        pieChart.setData(data);
+        pieChart.setDescription(getString(R.string.chart_desc));
+        dataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        pieChart.animateXY(1400, 1400);
+        data.setValueTextSize(13f);
+    }
+
+    private void updatePieChart() {
+        double newValue1 = Integer.parseInt(casesDataArray[0]);
+        double newValue2 = Integer.parseInt(casesDataArray[2]);
+        double newValue3 = Integer.parseInt(casesDataArray[4]);
+
+        Entry entry1 = new Entry((float) newValue1, 0);
+        Entry entry2 = new Entry((float) newValue2, 1);
+        Entry entry3 = new Entry((float) newValue3, 2);
+
+        yvalues.add(entry1);
+        yvalues.add(entry2);
+        yvalues.add(entry3);
+
+        pieChart.notifyDataSetChanged();
+        pieChart.invalidate();
     }
 }
