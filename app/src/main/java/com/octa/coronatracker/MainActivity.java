f9947068c3da1d761947895cadc816007dde28b5
imshubham <jangleshubham@ymail.com>
2020-07-30 17:18:41 +0530

    Added Piechart from MPAndroidChart library

--- app/src/main/java/com/octa/coronatracker/MainActivity.java
+++ app/src/main/java/com/octa/coronatracker/MainActivity.java
@@ -1,6 +1,8 @@
 package com.octa.coronatracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -9,11 +11,24 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.android.material.snackbar.Snackbar;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import static com.github.mikephil.charting.utils.ColorTemplate.rgb;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView, textViewActive, textViewDeath, textViewRecovered, textViewTotal,
@@ -21,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     String[] casesDataArray;
     int active, delActive, recovered, delRecovered, death, delDeath, total;
     Boolean flag = Boolean.FALSE;
+    PieChart pieChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
         textViewDeath = findViewById(R.id.textViewDeath);
         textViewDelDeath = findViewById(R.id.textViewDelDeath);
         textViewTotal = findViewById(R.id.textViewTotal);
+
+        pieChart = findViewById(R.id.piechart);
 
         getWebsite();
 
@@ -73,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
                             casesDataArray = textView.getText().toString().split(" ");
                             getData();
                             setData();
+                            setupPieChart();
                         }else{
                             textView.setVisibility(View.VISIBLE);
                         }
@@ -102,6 +121,34 @@ public class MainActivity extends AppCompatActivity {
         textViewTotal.setText("" + total);
     }
 
+    public void setupPieChart(){
+        ArrayList<PieEntry> yvalues = new ArrayList<>();
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        yvalues.add(new PieEntry(0 + active, "Active"));
+        yvalues.add(new PieEntry(0 + recovered, "Recovered"));
+        yvalues.add(new PieEntry(0 + death, "Death"));
+
+        PieDataSet dataSet = new PieDataSet(yvalues, "");
+        dataSet.setSelectionShift(5f);
+        dataSet.setSliceSpace(3f);
+
+        colors.add(ContextCompat.getColor(this, R.color.active));
+        colors.add(ContextCompat.getColor(this, R.color.recovered));
+        colors.add(ContextCompat.getColor(this, R.color.death));
+        dataSet.setColors(colors);
+
+        dataSet.setSelectionShift(10f);
+
+        pieChart.animateY(1400, Easing.EaseInOutQuad);
+        pieChart.setRotationEnabled(true);
+        pieChart.setHighlightPerTapEnabled(true);
+        pieChart.getDescription().setEnabled(false);
+
+        PieData data = new PieData(dataSet);
+        pieChart.setData(data);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -117,10 +164,15 @@ public class MainActivity extends AppCompatActivity {
                 getWebsite();
                 return true;
             case R.id.action_about:
-
+                about();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void about() {
+        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),"Made by Shubham Jangle",Snackbar.LENGTH_LONG);
+        snackbar.show();
     }
 }
